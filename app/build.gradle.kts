cc11e9d67dec1cae0c7aaba7d8d29aaedbb2b2ea
kusuma20 <kusumawan_23510005@stimata.ac.id>
2025-10-22 15:51:40 +0700

    commit update & Create data firestore

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,9 +59,9 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
 
     // Also add the dependencies for the Credential Manager libraries and specify their versions
-    implementation("androidx.credentials:credentials:1.3.0")
-    implementation("androidx.credentials:credentials-play-services-auth:1.3.0")
-    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.1")
+    implementation("androidx.credentials:credentials:1.2.2")
+    implementation("androidx.credentials:credentials-play-services-auth:1.2.2")
+    implementation("com.google.android.libraries.identity.googleid:googleid:1.1.0")
 
     // Declare the dependency for the Cloud Firestore library
     // When using the BoM, you don't specify versions in Firebase library dependencies
